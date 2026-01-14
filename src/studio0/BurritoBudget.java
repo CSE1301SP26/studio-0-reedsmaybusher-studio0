5b7c9a8a8e3e3019f7c1ad4fde4406d271eff288
muvlo <muvloroblox@gmail.com>
2026-01-14 15:05:26 -0600

    isaac test

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -9,7 +9,7 @@ public class BurritoBudget {
 		int burritosEatenWeek4 = 1;
 		
 		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
+		double averageWeeklyBurritos = totalBurritos / 4.00; // Needs to be divided by a double rather than into to prevent decimals from being omitted.
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
